80120b81e68c67e471e18d2ed786ea8a06617792
MrBuzzer <killyan.adalle@ynov.com>
2019-03-03 10:14:44 +0100

    1er push

---
+++ assets/php/head.php
@@ -0,0 +1,1 @@
+<link href="assets\css\aos.css" rel="stylesheet">
---
+++ assets/php/script.php
@@ -0,0 +1,7 @@
+<script src="assets/scripts/jquery.js"></script>
+<script src="assets/scripts/totop.js"></script>
+<script src="assets/scripts/aos.js"></script>
+<script type="text/javascript" src="assets/scripts/smooth.js"></script>
+<script>
+  AOS.init();
+</script>
